a3ef568691933236100d102f1451e80103d461fe
rana <ranasultan1188@gmail.com>
2025-11-23 16:15:42 +0300

    testing Ai feature

--- lib/ai_test.dart
+++ lib/ai_test.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gap/gap.dart';
@@ -31,12 +32,67 @@ class _AIImageAnalysisScreenState extends State<AIImageAnalysisScreen> {
       ],
     );
   }
+   void generateRecipe() async {
+    //  This function creates the recipe based on the user provided image. This function is where Gemini is used.
+   
+    // The instructions provided tell Gemini to create a recipe based on all logical ingredients observed in the image
+    String prompt = "You are an expert cook with detailed knowledge of making recipes. A user is interested in making recipes with a certain set of ingredients and have provided this. If no ingredients that can be used to realistically create food are provided, please state 'No ingredients in picture' to the user. Please generate a recipe that uses these ingredients. Please only return the following sections: Recipe Name, Ingredients, Complexity, Steps to Create. Please only return the recipe and do not return any other text in your response.";
+   
+    // The content is then passed to Gemini combining the instructions from the prompt and the image byte data
+    final content = [
+      Content.multi([
+      TextPart(prompt),
+      // The only accepted mime types are image/*.
+      DataPart('image/jpeg', imageBytes!),
+      // DataPart('image/jpeg', sconeBytes.buffer.asUint8List()),
+      ])
+    ];
+
+    // The model is then run and the recipe is generated
+    final recipe = await model.generateContent(content);
+
+    // Sets state to update the display of the app
+    setState(() {
+      
+      generatedText = recipe.text;
+
+    });
+  }
+ void pickFiles() async {
+    // This function enables users to pick files from their local directories
+    // This is used to allow users to select the image with the ingredients they want to cook with
+    // Uses the file picker Flutter library to achieve this
+
+    // The result variable contains the user's selected file
+    // The parameters here only allows the user to upload one image files
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowMultiple: false,
+      allowedExtensions: ['jpg', 'jpeg', 'png'],
+    );
+
+
+    if (result != null) {
+      PlatformFile file = result.files.first;
+
+      // Updates file name and data into appropriate variables when the user selects the files
+      setState(() {
+        selectedFileName = file.name;
+        imageBytes = file.bytes;
+      });
+
+    } else {
+      // User canceled the picker
+    }
+  }
 
   @override
   void initState() {
     super.initState();
     getAPIKey();
   }
+
+  
 
   @override
   Widget build(BuildContext context) {
@@ -54,10 +110,7 @@ class _AIImageAnalysisScreenState extends State<AIImageAnalysisScreen> {
               children: [
                 Expanded(
                   child: ElevatedButton(
-                    onPressed: () {
-                      // Put here a function to pick the image or file
-                    },
-
+                    onPressed: pickFiles,
                     style: ElevatedButton.styleFrom(
                       // backgroundColor: ,
                       // foregroundColor: ,
@@ -85,9 +138,7 @@ class _AIImageAnalysisScreenState extends State<AIImageAnalysisScreen> {
 
                 Gap(8),
                 ElevatedButton(
-                  onPressed: () {
-                    // Generate Result
-                  },
+                  onPressed: generateRecipe,
                   style: ElevatedButton.styleFrom(
                     // backgroundColor:
                     //foregroundColor:
@@ -122,3 +173,4 @@ class _AIImageAnalysisScreenState extends State<AIImageAnalysisScreen> {
     );
   }
 }
+
