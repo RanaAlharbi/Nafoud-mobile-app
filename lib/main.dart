b6ec6cba0d5b636e99750cca2fce41d1e3a7f394
rana <ranasultan1188@gmail.com>
2025-11-23 15:42:48 +0300

    profile screen ui design

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:final_project/features/home/presentation/pages/home_screen.dart';
+import 'package:final_project/features/profile/presentation_layer/pages/profile_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,12 +8,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const HomeScreen(),
+      home: const ProfileScreen(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-
