e070fca5a6dfd5963343a08250d97fe2219d9179
rana <ranasultan1188@gmail.com>
2025-11-28 20:38:50 +0300

    adding profile nav

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -32,7 +32,7 @@ class AppRoutes {
   static const navigationScreen = '/navigation_screen';
 
   static final GoRouter appRouter = GoRouter(
-    initialLocation: AppRoutes.signInScreen,
+    initialLocation: AppRoutes.navigationScreen,
     routes: [
       GoRoute(
         path: AppRoutes.chatScreen,
