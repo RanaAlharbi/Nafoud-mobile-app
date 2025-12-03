224a49e16d9eb2f2d3abaeed22458e6cacedf893
Elaf Abutaki <abutakielaf@gmail.com>
2025-12-03 13:21:31 +0300

    ....

--- lib/core/routes/router.dart
+++ lib/core/routes/router.dart
@@ -42,7 +42,7 @@ class AppRoutes {
   static const navigationScreen = '/navigation_screen';
 
   static final GoRouter appRouter = GoRouter(
-    initialLocation: AppRoutes.homeScreen,
+    initialLocation: AppRoutes.authenticationLandingScreen,
     /* GetIt.I.get<SupabaseClient>().auth.currentSession != null ? '/navigation_screen'
         : '/sign-in',*/
     routes: [
