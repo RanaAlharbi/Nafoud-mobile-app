840791506e2eb8e277224a087895237c7dab0770
Mohammed01py <j4r@outlook.sa>
2025-12-04 11:28:58 +0300

    Better navigation. Transparent & Colorful Icons
    - Transparent
    - Colorful Icons
    - ScreenUTIL

--- lib/features/navigation/presentation_layer/pages/navigation.dart
+++ lib/features/navigation/presentation_layer/pages/navigation.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:final_project/features/navigation/presentation_layer/cubit/navigation_cubit.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 
 class NavigationScreen extends StatelessWidget {
@@ -18,33 +19,74 @@ class NavigationScreen extends StatelessWidget {
             onTap: (value) => cubit.changeIndex(index: value),
             activeColor: Color(0xff656A53),
             inactiveColor: Color(0xFFB5B5B5),
+            backgroundColor: Color(0x00000000),
+            height: 70.h,
             items: [
               BottomNavigationBarItem(
                 icon: SvgPicture.asset(
                   'Assets/icons/nav_home.svg',
-                  width: 26,
-                  height: 26,
+                  width: 32.h,
+                  height: 32.h,
+                ),
+                activeIcon: SvgPicture.asset(
+                  'Assets/icons/nav_home.svg',
+                  width: 32.h,
+                  height: 32.h,
+                  colorFilter: ColorFilter.mode(
+                    Color(0xff656A53),
+                    BlendMode.srcIn,
+                  ),
                 ),
                 label: "Home",
               ),
-               BottomNavigationBarItem(
-                icon: SvgPicture.asset('Assets/icons/nav_gathering.svg',
-                width: 26,
-                height: 26,
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'Assets/icons/nav_gathering-cropped.svg',
+                  width: 34.h,
+                  height: 34.h,
+                ),
+                activeIcon: SvgPicture.asset(
+                  'Assets/icons/nav_gathering-cropped.svg',
+                  width: 34.h,
+                  height: 34.h,
+                  colorFilter: ColorFilter.mode(
+                    Color(0xff656A53),
+                    BlendMode.srcIn,
+                  ),
                 ),
                 label: "Gather",
               ),
-               BottomNavigationBarItem(
-               icon: SvgPicture.asset('Assets/icons/nav_ai.svg',
-                width: 26,
-                height: 26,
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'Assets/icons/nav_ai.svg',
+                  width: 32.h,
+                  height: 32.h,
+                ),
+                activeIcon: SvgPicture.asset(
+                  'Assets/icons/nav_ai.svg',
+                  width: 32.h,
+                  height: 32.h,
+                  colorFilter: ColorFilter.mode(
+                    Color(0xff656A53),
+                    BlendMode.srcIn,
+                  ),
                 ),
                 label: "Murshid",
               ),
-               BottomNavigationBarItem(
-              icon: SvgPicture.asset('Assets/icons/nav_user.svg',
-                width: 26,
-                height: 26,
+              BottomNavigationBarItem(
+                icon: SvgPicture.asset(
+                  'Assets/icons/nav_user.svg',
+                  width: 32.h,
+                  height: 32.h,
+                ),
+                activeIcon: SvgPicture.asset(
+                  'Assets/icons/nav_user.svg',
+                  width: 32.h,
+                  height: 32.h,
+                  colorFilter: ColorFilter.mode(
+                    Color(0xff656A53),
+                    BlendMode.srcIn,
+                  ),
                 ),
                 label: "Profile",
               ),
