b7f7b18b7f41e33d7431bebc91a5cf1b746ba8e7
Elaf Abutaki <abutakielaf@gmail.com>
2025-12-14 12:13:34 +0300

    icon

--- lib/features/sim_cards/presentation_layer/widgets/sim_card_widget.dart
+++ lib/features/sim_cards/presentation_layer/widgets/sim_card_widget.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -71,9 +72,9 @@ class SimCardWidget extends StatelessWidget {
                     ),
                   ),
                   const Icon(
-                    Icons.arrow_forward_ios,
+                    CupertinoIcons.arrow_up_right,
+                    color: Colors.white,
                     size: 16,
-                    color: Colors.grey,
                   ),
                 ],
               ),
