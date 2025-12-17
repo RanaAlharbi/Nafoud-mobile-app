e18d439c6c2918d3aaf13a0e19ae56124f290520
Mohammed01py <j4r@outlook.sa>
2025-12-17 08:51:33 +0300

    Adding translation so we can translate all the app
    - Fallback added so we can't get an error

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-// import 'package:easy_localization/easy_localization.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:final_project/core/di/configure_dependencies.dart';
 import 'package:final_project/core/routes/router.dart';
 import 'package:final_project/core/setup.dart';
@@ -9,28 +9,23 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp, // to make screen only vertical
   ]);
 
-  // await EasyLocalization.ensureInitialized();
   await setup();
   configureDependencies();
-  runApp(const MyApp());
-  // runApp(
-  //   EasyLocalization(
-  //     supportedLocales: const [
-  //       Locale('en'),
-  //       Locale('ar'),
-  //       Locale('fr'),
-  //       Locale('hi'),
-  //       Locale('ur'),
-  //     ],
-  //     path: 'assets/translations',
-  //     fallbackLocale: const Locale('en'),
-  //     child: const MyApp(),
-  //   ),
-  // );
+
+  runApp(
+    EasyLocalization(
+      supportedLocales: const [Locale('en', 'US'), Locale('ar', 'SA')],
+      path: 'assets/translations',
+      fallbackLocale: const Locale('en', 'US'),
+      useFallbackTranslations: true,
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -48,14 +43,14 @@ class MyApp extends StatelessWidget {
           routerConfig: AppRoutes.appRouter,
 
           // to avoid sys crashess due to cupertino vs material
-          localizationsDelegates: const [
+          localizationsDelegates: [
+            ...context.localizationDelegates,
             DefaultMaterialLocalizations.delegate,
             DefaultCupertinoLocalizations.delegate,
             DefaultWidgetsLocalizations.delegate,
           ],
-          // localizationsDelegates: context.localizationDelegates,
-          // supportedLocales: context.supportedLocales,
-          // locale: context.locale,
+          supportedLocales: context.supportedLocales,
+          locale: context.locale,
         );
       },
     );
